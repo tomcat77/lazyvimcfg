77413c7cb9f787230ce87becfee941c98b0fb032
Viacheslav Muravlev <vmuravlev@custis.ru>
2026-05-07 15:57:51 +0300

    Убрал catpuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,6 @@
 return {
     { "folke/tokyonight.nvim" },
     {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        opts = {
-            flavour = "latte",
-        },
-    },
-    {
         "LazyVim/LazyVim",
         opts = {
             colorscheme = "tokyonight-moon",
