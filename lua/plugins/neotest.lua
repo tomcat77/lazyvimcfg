ada0a720c3ede9fac90ac17b128b4a28da77e9ac
Viacheslav Muravlev <vmuravlev@custis.ru>
2026-05-27 18:11:16 +0300

    Настроен NeoTest + Java

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,12 +1,14 @@
 return {
     {
-      "nvim-neotest/neotest",
-      opts = {
-        adapters = {
-          ["neotest-java"] = {
-            -- put any neotest-java options here if needed
-          },
+        "nvim-neotest/neotest",
+        opts = {
+            adapters = {
+                ["neotest-java"] = {
+                    jvm_args = { "-Dapi.version=1.44" },
+                    env = { API_VERSION = "1.44" },
+                },
+            },
         },
-      },
     },
-  }
+}
+
